894e4aa1602547f59061b35fe10eba85321d528a
Amrit Raj <cnamritraj9123@gmail.com>
2026-05-21 09:37:47 +0530

    Time: 833 ms (47.83%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+with cte as(  # cte means common table expression 
+select *,sum(weight) over(order by turn rows between unbounded preceding and current row) as total 
+from Queue )
+
+select person_name
+from cte
+where total<=1000
+order by total desc limit 1
